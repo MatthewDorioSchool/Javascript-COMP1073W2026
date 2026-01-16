44bd27dd383991b6a43077939cee743031c51791
MatthewDorioSchool <200618886@student.georgianc.on.ca>
2026-01-16 00:16:20 -0500

    Generate Random Values JS code added

--- Lab-1/pet-profile.js
+++ Lab-1/pet-profile.js
@@ -2,8 +2,16 @@ var output = document.querySelector('p');
 //1. Declare and Initialize Variables
 
     let petName = 'Michelangelo';
-    let petType = 'Turtle';
+    let petType = ['Turtle','Shark','Toad',];
     let petAge = 15;
     let isHungry = true;
-    let favoriteActivities = ["Ninja Training","Eating Pizza","Skateboarding","Partying"];
-    let mood = "radical";
+    let favoriteActivities = ['Ninja Training','Eating Pizza','Skateboarding','Partying'];
+    let mood = ['radical','street wise','knuckle cracking'];
+//2. Generate Random Values
+    function getRandom(array){
+        let randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex]
+    }
+    let randomPetType = getRandom(petType);
+    let randomPetAge = Math.floor(Math.random() * 15) + 1;
+    let randomMood = getRandom(mood);
