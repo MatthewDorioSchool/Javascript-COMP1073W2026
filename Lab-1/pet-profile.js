b6c24287d68144cb9eb5e6fa21a48bc072786424
MatthewDorioSchool <200618886@student.georgianc.on.ca>
2026-01-15 23:28:45 -0500

    Declared and Initialized Variables

---
+++ Lab-1/pet-profile.js
@@ -0,0 +1,9 @@
+var output = document.querySelector('p');
+//1. Declare and Initialize Variables
+
+    let petName = 'Michelangelo';
+    let petType = 'Turtle';
+    let petAge = 15;
+    let isHungry = true;
+    let favoriteActivities = ["Ninja Training","Eating Pizza","Skateboarding","Partying"];
+    let mood = "radical";
